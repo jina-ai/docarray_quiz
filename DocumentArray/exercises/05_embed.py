008149335ea45c9964af0d48ee1ee4aec3a6c752
Johannes Messner <messnerjo@gmail.com>
2022-01-18 11:44:09 +0100

    update DocumentArray exercises and solutions

--- DocumentArray/exercises/05_embed.py
+++ DocumentArray/exercises/05_embed.py
@@ -11,7 +11,7 @@ model = torch.nn.Sequential(
     torch.nn.Linear(in_features=128, out_features=32))
 
 docs = DocumentArray.empty(10)
-docs.blobs = np.random.random([10, 128]).astype(np.float32)
+docs.tensors = np.random.random([10, 128]).astype(np.float32)
 ...
 
 assert docs.embeddings.shape == (10, 32)
